f86ee2d687a4569723d591158eb28a64bcd685d2
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-07 12:05:32 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,13 @@
 #include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
 void print_alphabet(void)
 {
 char lower = 'a';
-while (lower < 'z')
+while (lower <= 'z')
 {
 _putchar(lower);
 lower++;
