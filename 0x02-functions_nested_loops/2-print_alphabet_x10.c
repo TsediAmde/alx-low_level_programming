7862d94eb81951e4518ca0f82a6523d1d2bc61de
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-07 12:42:36 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet - check the code
+*
+* Return: Always void.
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i < 10)
+{
+char lower = 'a';
+while (lower <= 'z')
+{
+_putchar(lower);
+lower++;
+}
+i++;
+_putchar('\n');
+}
+}
