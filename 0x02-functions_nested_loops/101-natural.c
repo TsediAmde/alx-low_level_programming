aaf5d67d97ebfdee8b25ac5ddfc4c6d241916e20
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-08 11:40:02 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int result = 0;
+int counter = 1;
+while(counter < 1024)
+{
+if ((counter % 3 == 0) || (counter % 5 == 0))
+{
+result += counter;
+}
+counter++;
+}
+printf("%d", result);
+printf("\n");
+}
