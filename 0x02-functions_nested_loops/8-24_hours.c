f95a63b4d3caa7fba4d693be8062cceef77545cd
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-08 11:06:39 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 {
 int min;
 int hour = 0;
-while(hour < 24)
+while (hour < 24)
 {
 min=0;
-while(min < 60)
+while (min < 60)
 {
-_putchar((hour/10)+'0');
-_putchar((hour%10) +'0'); 
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0'); 
 _putchar(':');
-_putchar((min/10) +'0');
-_putchar((min%10) +'0'); 
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0'); 
 _putchar('\n');
 min++;
 }
