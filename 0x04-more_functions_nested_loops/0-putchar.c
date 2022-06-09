f77f9a68a44a170c8956f6512717149118f1a401
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-09 06:23:50 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - Entry point
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+char puter[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+int i = 0;
+while (i < 8)
+{
+_putchar(puter[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
