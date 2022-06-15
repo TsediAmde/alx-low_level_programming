b53c8068cd4bbf65ca97c444e6a5b66cac3bc1f6
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-15 07:08:48 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* reveserse_array - reverses the content of an array of integers
+* @a: character parameter
+* @n: character parameter
+*
+* Return: always 0
+*/
+void reverse_array(int *a, int n)
+{
+int i, rev;
+for (i = 0; i < n; i++)
+{
+n--;
+rev = a[i];
+a[i] = a[n];
+a[n] = rev;
+}
+}
