5be59d87e60302df8dc3d6e467f0e08a40fc1503
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-16 11:48:13 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,36 +1,27 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 /**
 * rot13 - function that encodes a string using rot13
 * @s: character parameter
 *
-* Return: s
+* Return: string
 */
 char *rot13(char *s)
-{  
-char* res = malloc(strlen(s));
-char* current_char = res;
-if(res != NULL)
 {
-strcpy(res, s); 
-while(*current_char != '\0')
+int a = 0;
+while (s[a])
 {
-if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90))
+while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
 {
-if(*current_char > 109 || (*current_char > 77 && *current_char < 91))
+if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
 {
-*current_char -= 13;
+s[a] -= 13;
+break;
 }
-else
-{
-*current_char += 13;
+s[a] += 13;
+break;
 }
+a++;
 }
-current_char++;
-}
-}
-s = res;
-return res;
+return (s);
 }
