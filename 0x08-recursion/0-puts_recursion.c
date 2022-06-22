bd3a7de5976b560fdaeb9c8ea59ec3f0e48d525a
TsediAmde <tsedeniaamde92@gmail.com>
2022-06-22 10:42:06 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,18 +4,18 @@
 *  _puts_recursion - function that prints a string
 * @s: string
 *
-* Return: success 
+* Return: success
 */
 void _puts_recursion(char *s)
 {
-if(*s)
+if (*s)
 {
 _putchar(*s);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
 else
 {
- _putchar('\n');
+_putchar('\n');
 }
 }
 
