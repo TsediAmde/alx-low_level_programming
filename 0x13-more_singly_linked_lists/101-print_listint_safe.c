a99f0c9350996de99fe99818724df500cd8b6035
TsediAmde <tsedeniaamde92@gmail.com>
2022-07-18 12:37:27 -0700

    Task 11-12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,44 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints list, avoids loop
- * @head: points to start of list
- * Return: number of nodes in size_t or exit 98
- */
+  * print_listint_safe - prints a linked list
+  *
+  * @head: head of list
+  * Return: number of nodes
+  */
 size_t print_listint_safe(const listint_t *head)
 {
-	/* declarations */
-	size_t nodeCount = 0;
-	/* check for null pointer */
+	size_t n = 0, i = 0;
+	const listint_t *loop;
+	const listint_t *h;
+
+	h = head;
+	loop = head;
 	while (head)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		nodeCount += 1;
-
-		if (head > head->next)
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == head->next)
 		{
-			head = head->next;
-		}
-		else
-		{
-			head = head->next;
-			printf("-> [%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *) head, head->n);
+			n++;
 			break;
 		}
+		head = head->next;
+		while (i < n && loop)
+		{
+			if (head == loop)
+			{
+				printf("-> [%p] %d\n", (void *) head, head->n);
+				if (head != head->next)
+					n++;
+				return (n);
+			}
+			loop = loop->next;
+			i++;
+		}
+		loop = h;
+		i = 0;
+		n++;
 	}
-	return (nodeCount);
+	return (n);
 }
